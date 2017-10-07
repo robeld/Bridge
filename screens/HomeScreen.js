a6d47d08e0091b63ed9c5867c10be373a6d466ba
robeld <robeld@stanford.edu>
2017-10-07 04:42:37 -0700

    aaa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -50,7 +50,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              C
+              Css 
             </Text>
           </View>
 
