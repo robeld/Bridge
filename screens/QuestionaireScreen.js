013c9975242eadc0fcaeed4c93ba7f764afbaecc
robinreversi <robincheong@gmail.com>
2017-10-07 23:43:12 -0700

    Finished questionaire

--- screens/QuestionaireScreen.js
+++ screens/QuestionaireScreen.js
@@ -6,37 +6,99 @@ import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-nativ
 
 
 export default class QuestionaireScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      gradYear: '',
+      sex: '',
+      ethnicity: '',
+      major: '',
+      courses: ''
+    };
+
+    this.handleAChange = this.handleAChange.bind(this);
+    this.handleBChange = this.handleBChange.bind(this);
+    this.handleCChange = this.handleCChange.bind(this);
+    this.handleDChange = this.handleDChange.bind(this);
+    this.handleEChange = this.handleEChange.bind(this);
+    this.handleFChange = this.handleFChange.bind(this);
+    this.handleGChange = this.handleGChange.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
   static navigationOptions = {
-    title: 'QuestionaireScreen',
+    title: 'QuestionaireScreen'
+  };
+
+  handleAChange(event) {
+    const value = event;;
+    this.state['name'] = value;
+  }
+
+  handleBChange(event) {
+    const value = event;
+    this.state['email'] = value;
+  }
+  handleCChange(event) {
+    const value = event;
+    this.state['gradYear'] = value;
+  }
+  handleDChange(event) {
+    const value = event;
+    this.state['sex'] = value;
+  }
+  handleEChange(event) {
+    const value = event;
+    this.state['ethnicity'] = value;
+  }
+  handleFChange(event) {
+    const value = event;
+    this.state['major'] = value;
+  }
+  handleGChange(event) {
+    const value = event;
+    this.state['courses'] = value;
+  }
+
+
+  handleSubmit() {
+    const { navigate } = this.props.navigation;
+    total = this.state['name'] + ',' + this.state['email'] + ',' + this.state['gradYear'] + ',' + this.state['sex'] + ',' + this.state['ethnicity'] + ',' + this.state['major'] + '/' + this.state['courses'];
+
+    navigate('Finding');
   };
 
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View>
         <FormLabel>Full Name</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleAChange}/>
 
         <FormLabel>Email ID</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleBChange}/>
 
         <FormLabel>Graduation Year</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleCChange}/>
 
         <FormLabel>Sex</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleDChange}/>
 
         <FormLabel>Ethnicity</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleEChange}/>
 
         <FormLabel>Major</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleFChange}/>
 
         <FormLabel>Courses List</FormLabel>
-        <FormInput onChangeText={(text) => this.setState({text})}/>
+        <FormInput onChangeText={this.handleGChange}/>
 
         <Button style={styles.button}
           large
+          backgroundColor="green"
+          onPress={()=>this.handleSubmit()}
           title="Submit!"
           />
       </View>
@@ -47,6 +109,5 @@ export default class QuestionaireScreen extends React.Component {
 const styles = StyleSheet.create({
   button: {
     marginTop: 20,
-    color: 'green'
   }
 });
